c9e1b0777106aaf98e0a305f2ec4ff320da0f017
Fernando <fernandofraga98@outloo.com>
2018-10-15 21:35:43 -0300

    Las funciones de conversion de longitud y latitud. Verificar si funcionan.

--- FunLonLat.c
+++ FunLonLat.c
@@ -1,4 +1,7 @@
-#include <stdio.h>
+
+
+/*Funciones para la longitud y latitud.
+Probar si funcionan! */
 
 #define GRADO_MIN  100
 #define LATITUD_MAX 9000.000
@@ -6,48 +9,40 @@
 #define MIN_POR_GRADO 60
 #define CARD_SUR "S"
 #define CARD_NORTE "N"
-//#define CARD_ESTE "E"
-//#define CARD_OESTE "W"
-//#define LONG_MIN
-//#define LONG_MAX
-#define VALOR_S_W -1
-#define VALOR_N_E 1
+#define CARD_ESTE "E"
+#define CARD_OESTE "W"
+#define LONG_MIN 0
+#define LONG_MAX 18000
+#define VALOR_S -1
+#define VALOR_N 1
+#define VALOR_W -1
+#define VALOR_E 1
 
-float convertirLat(const char lat[],const char cardinal);
+float convertirLon( const char lon[], const char cardinal){
 
-int main(void){
-char card;
-float latitud;	
-char s[8];
-	fgets(s,8, stdin);
-	card == getschar();
-	latitud = convertirLat(s, card);
-	printf("%f",latitud);
-
-	return 0;
-}
-
-/*float convertirLon( const char lon[], const char cardinal){
+float longitud;
 
 longitud = atoif(lon);
 
 if(longitud < LONG_MIN || longitud > LONG_MAX)
 	return NULL;
 
-if( cardinal != CARD_ESTE && != CARD_OESTE){
+if( cardinal != CARD_ESTE && cardinal != CARD_OESTE){
 	return NULL;
 }else if( cardinal == CARD_OESTE){
-	longitud = longitud * VALOR_S_W;
+	longitud = longitud * VALOR_W;
 }else{
-	longitud = longitud * VALOR_N_E;
+	longitud = longitud * VALOR_E;
 }
 
-return (int)longitud/LONG_MIN + (longitud%LONG_MIN)/MIN_POR_GRADO;
+return (int)longitud/GRADO_MIN + (longitud%GRADO_MIN)/MIN_POR_GRADO;
 }
-*/
+
 
 float convertirLat( const char lat[], const char cardinal){
+
 float latitud;
+
 latitud=atof(lat);
 
 if(latitud < LATITUD_MIN || latitud > LATITUD_MAX)
@@ -57,10 +52,10 @@ if (cardinal != CARD_NORTE && cardinal != CARD_SUR){
 	return 0;
 
 }else if(cardinal == CARD_SUR){
-	latitud= latitud*VALOR_S_W;
+	latitud= latitud*VALOR_S;
 
 }else{
-	latitud = latitud * VALOR_N_E;
+	latitud = latitud * VALOR_N;
 }
 
 return (int)(latitud/GRADO_MIN) + (float)(latitud%GRADO_MIN)/MIN_POR_GRADO;
