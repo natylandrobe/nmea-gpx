47befa56bdc96a596bd2810f8cbdd7e6711154ba
Fernando <fernandofraga98@outloo.com>
2018-10-25 13:22:51 -0300

    Cambios en la escritura del programa. Agrega archivos.h
    Viendo las correciones del tp agregue archivos de modularizacion para los defines y unos prototipos, de todas formas, no llegue a modularizar
    todo. Hay un error de compilacion en el archivo PrintFunc.c

---
+++ defines.h
@@ -0,0 +1,41 @@
+#define TRK "<trk>"
+#define TRK_C "</trk>"
+#define TRKSEG "<trkseg>"
+#define TRKSEG_C "</trkseg>"
+#define TRKPT_START "<trackpt lat=\""
+#define TRKPT_MID "\" lon=\""
+#define TRKPT_FIN "\">"
+#define ELE "<ele>"
+#define ELE_C "</ele>"
+#define TRKPT_C "</trkpt>"
+#define SPC  "   "
+#define SPC2 "      "
+#define SPC3 "         "
+#define T "T"
+#define Z "Z"
+#define XML "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+#define GPX "<gpx version=\"1.1\" creator=\"nmea-gpx\" xmlns=\"http://www.topografix.com/GPX/1/1\">"
+#define GPX_C "</gpx>"
+
+#define META "<metadata>"
+#define META_C "</metadata>"
+#define NAME "<name>"
+#define NAME_C "</name>"
+#define TIME "<time>"
+#define TIME_C "</time>"
+#define YEAR_DIFF 1900
+#define MON_DIFF 1
+
+#define HELP "Utilice los argumentos:\n-n o --name para ingresar el nombre de la ruta\n-f o --format aaaammdd para ingresar el anio, mes y dia\n-y o --year para ingresar el anio con la centuria\n-m o --month para ingresar el numero de mes y\n-d o --day para ingresar el numero de dia del mes \n"
+
+#define DIGITO_GRADO  100.0
+#define LAT_MAX 9000.000
+#define GRADO_MIN 0
+#define MIN_POR_GRADO 60
+#define CARD_SUR 'S'
+#define CARD_NORTE 'N'
+#define CARD_ESTE 'E'
+#define CARD_OESTE 'W'
+#define LONG_MAX 18000
+#define VALOR_SW -1
+#define ERR_LATLON 181
